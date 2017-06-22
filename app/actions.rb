5750aa7578801f6e119dd08fb957d48acc0b1fcb
Martina <martimarien@gmail.com>
2017-06-22 15:10:48 +0000

    update2

--- app/actions.rb
+++ app/actions.rb
@@ -122,6 +122,6 @@ end
 
 get "/:username" do
     @userId = User.find_by(username: params[:username])
-    @user_posts = @userId.posts_count
+    # @user_posts = @userId.posts_count
     erb(:userpage)
 end
