e766ad7fd905d56456495b4b818e7c4a81df9fe2
Martina <martimarien@gmail.com>
2017-06-22 17:32:53 +0000

    testign6

--- app/actions.rb
+++ app/actions.rb
@@ -120,9 +120,9 @@ delete '/likes/:id' do
     redirect(back)
 end
 
-get '/:username' do
-    @username = params[:username]
-    @userId = User.find_by(username: @username)
+get '/username' do
+    username = params[:username]
+    @userId = User.find_by(username: username)
     # @user_posts = @userId.posts_count
     erb(:userpage)
 end
